2fe95f42f4e5cd206697eb6f68840af455e90122
Ruslanito <breitovo76@gmail.com>
2017-01-20 21:39:05 +0300

    add new methods in ArraysBubble

--- src/ArraysBubble.java
+++ src/ArraysBubble.java
@@ -16,7 +16,7 @@ public class ArraysBubble {
             System.out.print(nums[i] + " ");
         }
 
-        for (a = 1; a < nums.length; a++)   // Пузырьковая сортировка
+        for (a = 1; a < nums.length; a++) // Пузырьковая сортировка
             for (b = nums.length - 1; b >= a; b--) {
                 if (nums[b - 1] > nums[b]) {
                     t = nums[b - 1];
