b85f07343b8b1c57a572bdfac7994229c4d55c24
Ruslanito <breitovo76@gmail.com>
2017-01-27 20:26:35 +0300

    add new methods in ArraysBubble

--- src/ArraysBubble2.java
+++ src/ArraysBubble2.java
@@ -3,18 +3,25 @@
  */
 public class ArraysBubble2 {
     public static void main(String[] args) {
-        int s = 5;// количество элементов массива
+        int s = 15;// количество элементов массива
         int mas[] = new int[s];// массив mass
         int i = 0;
         int numOfPair = 0;
 
+        System.out.println("Исходный случайный массив");
+        for (int j = 0; j < mas.length; j++) {
+            mas[j] = (int) Math.round(Math.random() * 1000);
+            System.out.print(mas[j] + " - ");
+        }
 
-
+        System.out.println();
+        System.out.println("****************");
         while (true) {
             if (mas[i] > mas[i + 1]) {
                 int q;
                 q = mas[i];
                 mas[i] = mas[i + 1];
+                mas [i+1] = q;
                 numOfPair = 0;
             } else {
                 numOfPair++;
@@ -24,9 +31,10 @@ public class ArraysBubble2 {
             if (i == mas.length - 1) {
                 i = 0;
             }
+
             for (int x : mas)
-                System.out.println(x);
-                System.out.println("numOfPair =" + numOfPair);
+                System.out.print(x + " ");
+            System.out.println("numOfPair =" + numOfPair);
         }
     }
 }
