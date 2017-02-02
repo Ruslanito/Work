fdae0b2844ec4692d0dfcd177dac24b48f464a74
Ruslanito <breitovo76@gmail.com>
2017-02-02 13:47:19 +0300

    add new methods in ArraysBubble

--- src/ArraysArraysList1.java
+++ src/ArraysArraysList1.java
@@ -8,25 +8,39 @@ public class ArraysArraysList1 {
     public static void main(String[] args) {
         ArrayList<Integer> arr;
         arr = new ArrayList();
+        int z=0;// переменная выхода из цикла while
 
         Scanner scanner = new Scanner(System.in);
 
         while   (scanner.hasNext()){
-            int q = scanner.nextInt();
-            arr.add(q);//добавдение элемента в колл. с помощью add
-            System.out.println(arr);//колл. в строчку
-            System.out.println("Количество элементов коллекции равно "+arr.size());//количество элементов массива
 
-            for (int i = 0; i < arr.size(); i++) {
-                System.out.println(arr.get(i));//колл. в столбик
+            if (z==5){
+                break;
+            } else {
+
+                int q = scanner.nextInt();
+                arr.add(q);//добавдение элемента в колл. с помощью add в конец колл.
+                System.out.println(arr);//колл. в строчку
+                System.out.println("Количество элементов коллекции равно " + arr.size());//количество элементов массива
+
+                for (int i = 0; i < arr.size(); i++) {
+                    System.out.println(arr.get(i));//колл. в столбик
+
+                }
 
             }
-            arr.set(0,999);//замена элемента колл.№0 на 999
-            System.out.println(arr);
+            z++;
+            System.out.println("Переменная цикла ="+z);
 
-            arr.remove(0);//удаление элемента колл.№0
-            System.out.println(arr);
         }
 
+        System.out.println("Вышли из цикла");
+        arr.set(2,999);//замена элемента колл.№2 на 999
+        System.out.println("Заменяем элемент колл. №2 на 999");
+        System.out.println(arr);
+
+        arr.remove(1);//удаление элемента колл.№1
+        System.out.println("Удаляем элемент колл. №1");
+        System.out.println(arr);
     }
 }
