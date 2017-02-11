02346c843fbeaf04f53cef27ba2dd53d682f1553
Ruslanito <breitovo76@gmail.com>
2017-02-11 11:50:22 +0300

    add new methods in ArraysBubble

--- src/Golovach/JavaCore/P1_5ArrayInvert.java
+++ src/Golovach/JavaCore/P1_5ArrayInvert.java
@@ -7,19 +7,33 @@ import java.util.Arrays;
  */
 public class P1_5ArrayInvert {
     public static void main(String[] args) {
-        int [] arr = new int [10];
+        int[] arr = new int[10];
+        int[] arr2 = new int[10];
+        int i, k, m;
 
+        m = arr2.length;
         System.out.println(arr.length);
-        for (int i=0; i < arr.length; i++){
-            arr [i] = (int) (Math.round(Math.random()*100) );
-            System.out.print(arr[i]+" ");
+        System.out.println(arr2.length);
+        for (i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.round(Math.random() * 100));
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
         System.out.println("Исходный массив имеет вид:");
         System.out.print(Arrays.toString(arr));
+        System.out.println();
+        System.out.print(Arrays.toString(arr2));
 
 
+        for (k = m - 1, i = 0; i < arr.length; i++) {
+            arr2[k - i] = arr[i];
+        }
 
+        System.out.println();
+        System.out.println("Исходный массив имеет вид:");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Инвертированный массив имеет вид:");
+        System.out.println(Arrays.toString(arr2));
 
     }
 }
