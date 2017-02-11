7c4c8fe524e5c5de95dea021c22b8cbcab084318
Ruslanito <breitovo76@gmail.com>
2017-02-11 21:13:52 +0300

    add TkachYura

--- src/TkachYura/Practice/DealsInfo.java
+++ src/TkachYura/Practice/DealsInfo.java
@@ -1,5 +1,7 @@
 package TkachYura.Practice;
 
+import java.util.Date;
+
 /**
  * Created by Ruslanito on 11.02.2017.
  */
@@ -20,8 +22,9 @@ public class DealsInfo {
 
     public class Product {// товары отражённые в сделке
         private String title;
-        private double price;
+        private int price;
         private int quantity;
+        int cost;
 
         public String getTitle() {
             return title;
@@ -35,7 +38,7 @@ public class DealsInfo {
             return price;
         }
 
-        public void setPrice(double newprice) {
+        public void setPrice(int newprice) {
             this.price = newprice;
         }
 
@@ -46,6 +49,51 @@ public class DealsInfo {
         public void setQuantity(int newquantity) {
             this.quantity = newquantity;
         }
+
+        public  int getCost (){
+            cost = price*quantity;
+            return cost;
+        }
     }
+
+    public class Deal {
+        private Date date = new Date();
+        private Party seller;
+        private Party buyer;
+        private Product[] products;
+        int summ = 0;
+
+        public Date getDate() {
+            return date;
+        }
+
+        public String getSeller() {
+            return String.valueOf(seller);
+        }
+
+        public String getBuyer() {
+            return String.valueOf(buyer);
+        }
+
+        public Product[] getProducts() {
+            return products;
+        }
+
+        //конструктор сделок
+        public Deal (Party buyer, Party seller, Product[] product){
+            date = new Date();
+            this.seller = seller;
+            this.buyer = buyer;
+            this.products = product;
+        }
+
+        public int getSumm(){
+            for (Product prod: products) {
+                summ = summ + products.getCost();
+            }
+            return summ;
+        }
+    }
+
 
 }
