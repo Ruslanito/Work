2d5db695f9962fa84718dd25b40a3b402302e12c
Ruslanito <breitovo76@gmail.com>
2017-02-24 16:01:55 +0300

    add  Core_String

---
+++ src/com/ruslanito/Core_Srting/Srting_Summ.java
@@ -0,0 +1,27 @@
+package com.ruslanito.Core_Srting;
+
+/**
+ * Created by Ruslanito on 24.02.2017.
+ */
+public class Srting_Summ {
+    public static void main(String[] args) {
+        String s1 = "Первая строка ";
+        String s2 = "Вторая строка ";
+        String s3 = s1 + s2;
+        System.out.println("Итоговая строка имеет вид: " + s3);
+
+        System.out.println("StringBuffer ()");
+        StringBuffer s = new StringBuffer();
+        s.append("Hello ");
+        s.append("World!");
+        String s4 = s.toString();
+        System.out.println("StringBuffer () " + s4);
+
+        System.out.println("concat()");
+        String s5 = "Первая строка ";
+        String s6 = s5.concat("Вторая строка");
+        System.out.println("concat() " + s6);
+        System.out.println(s5.concat("Вторая строка"));
+    }
+
+}
---
+++ src/com/ruslanito/Core_Srting/String_ValueOf.java
@@ -0,0 +1,7 @@
+package com.ruslanito.Core_Srting;
+
+/**
+ * Created by Ruslanito on 24.02.2017.
+ */
+public class String_ValueOf {
+}
