fc0094a256239c0de55d572d44e10e1dc117c7d1
Ruslanito <breitovo76@gmail.com>
2017-03-08 10:47:17 +0300

    add new methods in ArraysBubble

---
+++ src/Golovach/JavaCore/P1_5ArrayInvert.java
@@ -0,0 +1,34 @@
+package Golovach.JavaCore;
+
+import java.util.Arrays;
+
+/**
+ * Created by Ruslanito on 08.02.2017.
+ */
+public class P1_5ArrayInvert {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        int[] arr2 = new int[10];
+        int i, k, m;
+
+        m = arr2.length;
+        System.out.println(arr.length);
+        System.out.println(arr2.length);
+        for (i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.round(Math.random() * 100));
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Исходный массив имеет вид:");
+        System.out.print(Arrays.toString(arr));
+        System.out.println();
+        System.out.print(Arrays.toString(arr2));
+
+        //for (k = 0; k < arr2.length; k--)
+         //   for (k<m ; k = 0 ; k--){
+
+       //}
+
+
+    }
+}
